1f3298fd4730737cdb8e2fc1a74532f0267f6c30
Rachelle <rkozuma@hawaii.edu>
2020-02-04 10:10:05 -1000

    Completed Parts 1-4

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -2,3 +2,38 @@ import UIKit
 
 var str = "Hello, playground"
 
+var goodGrade = "I've been doing all my work"
+
+// bellow is the if statement... If the statement is true, the code in the brackets will run.
+if goodGrade == "I've been doing all my work"
+{
+    print("I should be getting an A ath this point")
+}
+// if the statement is false it will print the code in the else bracket
+else
+{
+    print("I need to work harder in this class")
+}
+
+
+
+/*
+boolean and if statement combined.
+*/
+
+
+// creating the boolean statement
+
+var gradeA = true
+
+// creating the if-else statement
+
+if gradeA
+{
+    print("I really like this class")
+}
+
+else
+{
+    print("I'm not sure about coding...")
+}
